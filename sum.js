624447b795844c9f43284dbe684bd0557e5a6ad3
Sagar-DU <nahidhasansagar@gmail.com>
2023-06-11 00:57:23 +0600

    sum with recursion

--- sum.js
+++ sum.js
@@ -1,7 +1,16 @@
 // Sum with loop
-let sum = 0;
+/*let sum = 0;
 for (let i = 5; i > 0; i--) {
     sum += i;
 }
-
 console.log(sum);
+*/
+
+// Sum with recursive funciton
+function sum(i){
+    if (i == 1){
+        return 1;
+    }
+    return i + sum(--i);
+}
+console.log(sum(100));
