2533371131863a0b60c7b6f078efcce1f68075bb
Sagar-DU <nahidhasansagar@gmail.com>
2023-06-11 01:59:47 +0600

    new search and find problem started

---
+++ search.js
@@ -0,0 +1,37 @@
+let products = [
+    {
+        name:"iPhone 14", price: 100000
+    },
+    {
+        name:"iPhone 14 plus", price: 120000
+    },
+    {
+        name:"iPhone 14 pro", price: 140000
+    },
+    {
+        name:"iPhone 14 pro MAX", price: 150000
+    },
+    {
+        name:"M2 Macbook Air 13", price: 100000
+    },
+    {
+        name:"M2 Macbook Air 15", price: 120000
+    },
+    {
+        name:"Macbook pro 13", price: 130000
+    },
+    {
+        name:"Macbook pro 14", price: 150000
+    },
+    {
+        name:"Macbook pro 16", price: 170000
+    }
+]
+
+function search(products, searchText){
+    for (const porduct of porducts) {
+        console.log(product);
+    }
+}
+
+search(products);
